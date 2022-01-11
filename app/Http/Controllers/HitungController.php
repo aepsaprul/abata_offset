d82b162fa5f5d8e8da6060a7db0b4b470d934f6e
aep <aepsaprul18@gmail.com>
2022-01-11 15:14:51 +0700

    hitung plano

--- app/Http/Controllers/HitungController.php
+++ app/Http/Controllers/HitungController.php
@@ -3,11 +3,119 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
 
 class HitungController extends Controller
 {
     public function paperBag()
     {
         return view('pages.hitung_paper_bag.index');
+    }
+
+    public function hitung()
+    {
+        // ukuran real
+        $panjang = 28;
+        $lebar = 42;
+        $tinggi = 4;
+
+        // title
+        $lipatan_bawah = $tinggi - 2;
+        $lipatan_atas = $lipatan_bawah * 0.5;
+        $lipatan_lem = $lipatan_atas;
+
+        // ukuran plat
+        $plat_lebar = $lipatan_atas + $lebar + $lipatan_bawah;
+        $plat_panjang = 2 * ($panjang + $tinggi) + $lipatan_lem;
+
+
+        // ukuran plano kecil
+        $plano_panjang_1 = 100;
+        $plano_lebar_1 = 65;
+
+        // hitung plano kecil
+            // lebar x panjang (standar)
+            $hitung_lebar_plat_lebar_plano_kecil = $plano_lebar_1 / $plat_lebar;
+            $hitung_panjang_plat_panjang_plano_kecil = $plano_panjang_1 / $plat_panjang;
+            // hasil
+            $hasil_1 = floor($hitung_lebar_plat_lebar_plano_kecil) * floor($hitung_panjang_plat_panjang_plano_kecil);
+
+            // lebar x panjang (di tukar)
+            $hitung_lebar_plat_panjang_plano_kecil = $plano_lebar_1 / $plat_panjang;
+            $hitung_panjang_plat_lebar_plano_kecil = $plano_panjang_1 / $plat_lebar;
+            // hasil
+            $hasil_2 = floor($hitung_lebar_plat_panjang_plano_kecil) * floor($hitung_panjang_plat_lebar_plano_kecil);
+
+
+        // ukuran plano besar
+        $plano_panjang_2 = 109;
+        $plano_lebar_2 = 79;
+
+        // hitung plano besar
+            // lebar x panjang (standar)
+            $hitung_lebar_plat_lebar_plano_besar = $plano_lebar_2 / $plat_lebar;
+            $hitung_panjang_plat_panjang_plano_besar = $plano_panjang_2 / $plat_panjang;
+            // hasil
+            $hasil_3 = floor($hitung_lebar_plat_lebar_plano_besar) * floor($hitung_panjang_plat_panjang_plano_besar);
+
+            // lebar x panjang (di tukar)
+            $hitung_lebar_plat_panjang_plano_besar = $plano_lebar_2 / $plat_panjang;
+            $hitung_panjang_plat_lebar_plano_besar = $plano_panjang_2 / $plat_lebar;
+            // hasil
+            $hasil_4 = floor($hitung_lebar_plat_panjang_plano_besar) * floor($hitung_panjang_plat_lebar_plano_besar);
+
+        // result
+        $array = array(1 => $hasil_1, 2 => $hasil_2, 3 => $hasil_3, 4 => $hasil_4);
+        $maxValue = max($array);
+        $maxIndex = array_search(max($array), $array);
+
+        if ($maxIndex == 1 || $maxIndex == 2) {
+            $maxx = "Plano Kecil";
+        }
+        if ($maxIndex == 3 || $maxIndex == 4) {
+            $maxx = "Plano Besar";
+        }
+
+        return $maxx . " dapat " . $maxValue;
+        echo "<br>";
+        echo "<br>";
+        echo "<br>";
+
+
+        echo "panjang plat = " . $plat_panjang;
+        echo "<br>";
+        echo "lebar plat = " . $plat_lebar;
+        echo "<br>";
+        echo "- hitung lebar plano " . $plano_lebar_1 . " / " . $plat_lebar . " = " . floor($hitung_lebar_plat_lebar_plano_kecil);
+        echo "<br>";
+        echo "- hitung panjang plano " . $plano_panjang_1 . " / " . $plat_panjang . " = " . floor($hitung_panjang_plat_panjang_plano_kecil);
+        echo "<br>";
+        echo "hasil plano kecil = " . floor($hasil_1);
+        echo "<br>";
+        echo "- hitung lebar plano " . $plano_lebar_1 . " / " . $plat_panjang . " = " . floor($hitung_lebar_plat_panjang_plano_kecil);
+        echo "<br>";
+        echo "- hitung panjang plano " . $plano_panjang_1 . " / " . $plat_lebar . " = " . floor($hitung_panjang_plat_lebar_plano_kecil);
+        echo "<br>";
+        echo "hitung plano kecil = " . floor($hasil_2);
+        echo "<br>";
+        echo "<hr>";
+
+        echo "panjang plat = " . $plat_panjang;
+        echo "<br>";
+        echo "lebar plat = " . $plat_lebar;
+        echo "<br>";
+        echo "- hitung lebar plano " . $plano_lebar_2 . " / " . $plat_lebar . " = " . floor($hitung_lebar_plat_lebar_plano_besar);
+        echo "<br>";
+        echo "- hitung panjang plano " . $plano_panjang_2 . " / " . $plat_panjang . " = " . floor($hitung_panjang_plat_panjang_plano_besar);
+        echo "<br>";
+        echo "hasil plano besar = " . floor($hasil_3);
+        echo "<br>";
+        echo "- hitung lebar plano " . $plano_lebar_2 . " / " . $plat_panjang . " = " . floor($hitung_lebar_plat_panjang_plano_besar);
+        echo "<br>";
+        echo "- hitung panjang plano " . $plano_panjang_2 . " / " . $plat_lebar . " = " . floor($hitung_panjang_plat_lebar_plano_besar);
+        echo "<br>";
+        echo "hitung plano besar = " . floor($hasil_4);
+        echo "<br>";
+        echo "<hr>";
     }
 }
--- resources/views/pages/hitung_paper_bag/index.blade.php
+++ resources/views/pages/hitung_paper_bag/index.blade.php
@@ -16,125 +16,123 @@
                         <h2>Pehitungan Paper Bag</h2>
                         <div class="clearfix"></div>
                     </div>
-                    <div class="x_content">
-                        <br />
-                    <form id="form-kalender-dinding">
-                        @csrf
-                        <div class="row">
-                            <div class="col-md-6">
-                                <div class="row">
-                                    <div class="col-md-12">
-                                        <div class="form-group row mb-3">
-                                            <label for="jml_warna" class="col-sm-4 col-form-label">Jumlah Cetak</label>
-                                            <div class="col-sm-8">
-                                                <input type="text" class="form-control form-control-sm">
+                    <div class="x_content mt-3 mb-3">
+                        <form id="form-kalender-dinding">
+                            @csrf
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <div class="form-group row mb-3">
+                                                <label for="jml_warna" class="col-sm-4 col-form-label">Jumlah Cetak</label>
+                                                <div class="col-sm-8">
+                                                    <input type="text" class="form-control form-control-sm">
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
-                                    <div class="col-md-12">
-                                        <div class="form-group row mb-3">
-                                            <label for="jml_halaman_kalender" class="col-sm-4 col-form-label">Ukuran Paper Bag</label>
-                                            <div class="col-sm-8">
-                                                <div class="row">
-                                                    <div class="col-sm-4">
-                                                        <input type="text" class="form-control form-control-sm" placeholder="Panjang">
-                                                    </div>
-                                                    <div class="col-sm-4">
-                                                        <input type="text" class="form-control form-control-sm" placeholder="Lebar">
-                                                    </div>
-                                                    <div class="col-sm-4">
-                                                        <input type="text" class="form-control form-control-sm" placeholder="Tinggi">
+                                        <div class="col-md-12">
+                                            <div class="form-group row mb-3">
+                                                <label for="jml_halaman_kalender" class="col-sm-4 col-form-label">Ukuran Paper Bag</label>
+                                                <div class="col-sm-8">
+                                                    <div class="row">
+                                                        <div class="col-sm-4">
+                                                            <input type="text" class="form-control form-control-sm" placeholder="Panjang">
+                                                        </div>
+                                                        <div class="col-sm-4">
+                                                            <input type="text" class="form-control form-control-sm" placeholder="Lebar">
+                                                        </div>
+                                                        <div class="col-sm-4">
+                                                            <input type="text" class="form-control form-control-sm" placeholder="Tinggi">
+                                                        </div>
                                                     </div>
                                                 </div>
                                             </div>
                                         </div>
-                                    </div>
-                                    <div class="col-md-12">
-                                        <div class="form-group row mb-3">
-                                            <label for="jenis_kertas" class="col-sm-4 col-form-label">Jenis Kertas</label>
-                                            <div class="col-sm-8">
-                                                <select class="form-control form-control-sm" id="jenis_kertas" name="jenis_kertas">
-                                                    <option value="">--Pilih Kertas--</option>
-                                                    @foreach ($jenis_kertas as $item_jenis_kertas)
-                                                        <option value="{{ $item_jenis_kertas->id }}">{{ $item_jenis_kertas->nama_kertas }} {{ $item_jenis_kertas->gramasi }}</option>
-                                                    @endforeach
-                                                </select>
+                                        <div class="col-md-12">
+                                            <div class="form-group row mb-3">
+                                                <label for="jenis_kertas" class="col-sm-4 col-form-label">Jenis Kertas</label>
+                                                <div class="col-sm-8">
+                                                    <select class="form-control form-control-sm" id="jenis_kertas" name="jenis_kertas">
+                                                        <option value="">--Pilih Kertas--</option>
+                                                        @foreach ($jenis_kertas as $item_jenis_kertas)
+                                                            <option value="{{ $item_jenis_kertas->id }}">{{ $item_jenis_kertas->nama_kertas }} {{ $item_jenis_kertas->gramasi }}</option>
+                                                        @endforeach
+                                                    </select>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-12">
+                                            <div class="form-group row mb-3">
+                                                <label for="laminasi" class="col-sm-4 col-form-label">Laminasi</label>
+                                                <div class="col-sm-8">
+                                                    <select class="form-control form-control-sm" id="laminasi" name="laminasi">
+                                                    </select>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-12">
+                                            <div class="form-group row mb-3">
+                                                <label for="laminasi" class="col-sm-4 col-form-label">Biaya Finishing</label>
+                                                <div class="col-sm-8">
+                                                    <input type="text" name="" id="" class="form-control form-control-sm">
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="col-md-12">
-                                        <div class="form-group row mb-3">
-                                            <label for="laminasi" class="col-sm-4 col-form-label">Laminasi</label>
-                                            <div class="col-sm-8">
-                                                <select class="form-control form-control-sm" id="laminasi" name="laminasi">
-                                                </select>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <div class="form-group row mb-3">
+                                                <label for="biaya_potong" class="col-sm-4 col-form-label">Biaya Potong</label>
+                                                <div class="col-sm-8">
+                                                    <input type="text" class="form-control form-control-sm" id="biaya_potong" name="biaya_potong">
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
-                                    <div class="col-md-12">
-                                        <div class="form-group row mb-3">
-                                            <label for="laminasi" class="col-sm-4 col-form-label">Biaya Finishing</label>
-                                            <div class="col-sm-8">
-                                                <input type="text" name="" id="" class="form-control form-control-sm">
+                                        <div class="col-md-12">
+                                            <div class="form-group row mb-3">
+                                                <label for="biaya_design" class="col-sm-4 col-form-label">Biaya Design</label>
+                                                <div class="col-sm-8">
+                                                    <input type="text" class="form-control form-control-sm" id="biaya_design" name="biaya_design">
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-12">
+                                            <div class="form-group row mb-3">
+                                                <label for="biaya_akomodasi" class="col-sm-4 col-form-label">Biaya Akomodasi</label>
+                                                <div class="col-sm-8">
+                                                    <input type="text" class="form-control form-control-sm" id="biaya_akomodasi" name="biaya_akomodasi">
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-12">
+                                            <div class="form-group row mb-3">
+                                                <label for="jml_cetak" class="col-sm-4 col-form-label">Lain - lain</label>
+                                                <div class="col-sm-8">
+                                                    <input type="text" class="form-control form-control-sm" id="jml_cetak" name="jml_cetak">
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-12">
+                                            <div class="form-group row mb-3">
+                                                <label for="nama_file" class="col-sm-4 col-form-label">Nama File</label>
+                                                <div class="col-sm-8">
+                                                    <input type="text" class="form-control form-control-sm" id="nama_file" name="nama_file" required>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-md-6">
-                                <div class="row">
-                                    <div class="col-md-12">
-                                        <div class="form-group row mb-3">
-                                            <label for="biaya_potong" class="col-sm-4 col-form-label">Biaya Potong</label>
-                                            <div class="col-sm-8">
-                                                <input type="text" class="form-control form-control-sm" id="biaya_potong" name="biaya_potong">
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-12">
-                                        <div class="form-group row mb-3">
-                                            <label for="biaya_design" class="col-sm-4 col-form-label">Biaya Design</label>
-                                            <div class="col-sm-8">
-                                                <input type="text" class="form-control form-control-sm" id="biaya_design" name="biaya_design">
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-12">
-                                        <div class="form-group row mb-3">
-                                            <label for="biaya_akomodasi" class="col-sm-4 col-form-label">Biaya Akomodasi</label>
-                                            <div class="col-sm-8">
-                                                <input type="text" class="form-control form-control-sm" id="biaya_akomodasi" name="biaya_akomodasi">
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-12">
-                                        <div class="form-group row mb-3">
-                                            <label for="jml_cetak" class="col-sm-4 col-form-label">Lain - lain</label>
-                                            <div class="col-sm-8">
-                                                <input type="text" class="form-control form-control-sm" id="jml_cetak" name="jml_cetak">
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-12">
-                                        <div class="form-group row mb-3">
-                                            <label for="nama_file" class="col-sm-4 col-form-label">Nama File</label>
-                                            <div class="col-sm-8">
-                                                <input type="text" class="form-control form-control-sm" id="nama_file" name="nama_file" required>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
+                            <button type="submit" class="btn btn-primary btn-sm btn-block">Hitung</button>
+                        </form>
+                        <form id="kalender-dinding-detail" action="{{ route('home.produk.kalender_dinding_detail') }}" method="POST">
+                            @csrf
+                            <div class="row hasil_hitung">
+                                {{-- hasil hitung di jquery  --}}
                             </div>
-                        </div>
-                        <button type="submit" class="btn btn-primary btn-sm btn-block">Hitung</button>
-                    </form>
-                    <br>
-                    <form id="kalender-dinding-detail" action="{{ route('home.produk.kalender_dinding_detail') }}" method="POST">
-                        @csrf
-                        <div class="row hasil_hitung">
-                            {{-- hasil hitung di jquery  --}}
-                        </div>
-                    </form>
+                        </form>
                     </div>
                 </div>
             </div>
